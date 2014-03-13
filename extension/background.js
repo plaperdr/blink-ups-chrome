475a912fce2120686252103e41ea1ee61d817c85
Pierre Laperdrix <laperdrix.p@gmail.com>
2014-03-13 16:46:45 +0100

    Initial commit with partial support of tabs

---
+++ extension/background.js
@@ -0,0 +1,56 @@
+var port = chrome.runtime.connectNative('com.cip.accessor');
+
+//When the extension receives a message, all opened tabs
+//are closed to open the new ones
+port.onMessage.addListener(function(data) {
+	console.log("Received" + data);
+	
+	//Clean Open tabs before opening the new ones
+	chrome.tabs.query({}, function(tabs){
+	    for (var i = 1; i < tabs.length; i++) {
+	    	chrome.tabs.remove(tabs[i].id);                         
+	    }
+	});
+	
+	chrome.tabs.query({},function(tab){
+		chrome.tabs.update(tab[0].id,{"url": data[0].url});
+	});
+	for(i=1 ; i< data.length ; i++){
+		chrome.tabs.create({"url": data[i].url});
+	}
+  
+});
+port.onDisconnect.addListener(function() {
+  console.log("Disconnected");
+});
+
+//Add listeners
+chrome.tabs.onCreated.addListener(tabCreated);
+function tabCreated(){
+	sendOpenTabs();
+}
+
+chrome.tabs.onRemoved.addListener(tabRemoved);
+function tabRemoved(){
+	sendOpenTabs();
+}
+
+chrome.tabs.onUpdated.addListener(tabUpdated);
+function tabUpdated(){
+	sendOpenTabs();
+}
+
+chrome.windows.onRemoved.addListener(windowClosed);
+function windowClosed(){
+	sendOpenTabs();
+}
+
+
+function sendOpenTabs(){
+	chrome.tabs.query({}, function(tabs){
+		port.postMessage(tabs);
+	});
+}
+
+
+
